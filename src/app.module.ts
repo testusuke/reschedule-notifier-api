c9444bb5c0137152ac4988c57a6a947f2182140f
testusuke <silverbro007@icloud.com>
2022-12-13 00:40:19 +0900

    fix: users route was mapped twice

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
-import { UsersService } from './users/users.service';
-import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController, UsersController],
-  providers: [AppService, PrismaService, UsersService],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
